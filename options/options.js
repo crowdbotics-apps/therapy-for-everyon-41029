15f55e73ff95f2e9a91a6cc243b4ea4242990adc
Crowdbotics <team@crowdbotics.com>
2023-05-26 22:52:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "therapy-for-everyon-41029",
+  url: "https://therapy-for-everyon-41029.botics.co",
+  api: "https://therapy-for-everyon-41029.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
